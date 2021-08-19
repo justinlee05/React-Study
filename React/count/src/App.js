4b749b2627ad07821ee5b0a6ff8a96e75e24164c
justinlee05 <justinlee05@naver.com>
2021-08-19 16:49:46 +0900

    카운터 적용

--- React/count/src/App.js
+++ React/count/src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './App.css';
 import './App.css'
+import Counter from './Products/Counter';
 
 function App() {
+
+  const style = {
+    backgroundColor:'gray',
+    display:'flex',
+    flexDirection:'column',
+    width:'100%',
+    height:'100vh'
+}
+
   return (
-    <h1>카운트 만들기</h1>
+    <div style={style}>
+      <h1>카운트 만들기</h1>
+      <Counter/>
+    </div>
   );
 }
 
