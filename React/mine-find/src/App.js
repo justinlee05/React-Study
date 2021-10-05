b9d00a455472972ee7ec22500ed4efcb3d33099f
justinlee05 <justinlee05@naver.com>
2021-10-05 20:13:29 +0900

    지뢰찾기 연속오픈 구현 성공
    으핳 십알 난 천재야

--- React/mine-find/src/App.js
+++ React/mine-find/src/App.js
@@ -1,99 +1,144 @@
-import React, { useState } from 'react';
-import Board from './Board';
-import { S } from './styles';
+import React, { useEffect, useState } from "react";
+import { render } from "react-dom";
+import Board from "./Board";
+import { S } from "./styles";
 
-function App(){
-  
+function App() {
   const subarr = [];
-  for (let i=0;i<10;i++){
-    for (let k=0;k<10;k++){
-        subarr.push({
-            posX:k,
-            posY:i,
-            pos:k+','+i,
-            index:i*10+k,
-            number:0,
-            clicked:false
-        })
+  for (let i = 0; i < 10; i++) {
+    for (let k = 0; k < 10; k++) {
+      subarr.push({
+        posX: k,
+        posY: i,
+        pos: k + "," + i,
+        index: i * 10 + k,
+        number: 0,
+        clicked: false,
+        flaged: false,
+      });
     }
   }
 
-  const [mainarr ,setArr] = useState(subarr);
+  const getRandomInt = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //최댓값은 제외, 최솟값은 포함
+  };
 
-  const onClick = (pos) => {
-    setArr(
-      mainarr.map(index=>index.index === pos ? {...index,clicked:!index.clicked} : {...index})
-    )
-    console.log(mainarr[pos].number)
-    console.log(pos)
-    console.log(mainarr[pos].pos)
-    addBomb(1,2)
-  }
-
-  const addBomb = (posX,posY) =>{
-    setArr(
-      mainarr.map(index => 
-          index.index === (posY*10+posX) ? {...index,number:-1} : {...index}
-      )
-    )
-    console.log(`X : ${posX} Y : ${posY}`);
-  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  const rendering = () => {
-    const result = [];
-    const count = [0,1,2,3,4,5,6,7,8,9];
-    
-    for(let i=0;i<10;i++){
-      const temp =(
-      <S.Row>
-        {
-        count.map( index=>
-          <S.Block index={mainarr[10*i+index].number} onClick={()=> {onClick(10*i+index)}}>
-            {mainarr[10*i+index].number>0 && mainarr[10*i+index].clicked ? mainarr[10*i+index].number : ''}
-          </S.Block>
-        )
+  const Test = () => {
+    const tempnum = getRandomInt(0, 100);
+    console.log(tempnum);
+    const tempX = tempnum % 10;
+    const tempY = parseInt(tempnum / 10);
+    console.log(tempX, tempY);
+    subarr[tempnum].number = -1;
+    for (let i = -1; i < 2; i++) {
+      for (let j = -1; j < 2; j++) {
+        if (
+          tempX + j < 0 ||
+          tempX + j > 9 ||
+          tempY + i < 0 ||
+          tempY + i > 9 ||
+          subarr[(tempY + i) * 10 + (tempX + j)].number === -1
+        ) {
+        } else {
+          subarr[(tempY + i) * 10 + (tempX + j)].number += 1;
         }
-      </S.Row>  
-      )
-      
-      result.push(temp)
-      
-      
+      }
     }
-    return result
-  }
-  console.log(rendering());
-  
-  return(
-    <>
-    {rendering()}   
-    </>
-  )
+  };
 
+  useEffect(() => {
+    for (let i = 0; i < 10; i++) {
+      Test();
+    }
+  }, []);
+
+  const [mainarr, setArr] = useState(subarr);
+  const checked = [];
+
+  function findEmpty(posX, posY) {
+    console.log("findEmpty(", posX, posY, ") 호출됨");
+
+    if (posX < 0 || posX > 9 || posY < 0 || posY > 9) {
+      console.log("상하좌우 없음,", posX, posY);
+      return;
+    }
+    // 상하좌우 없는 칸은 안 열기
+
+    if (checked.find((index) => index === posY * 10 + posX)) {
+      console.log("이미 확인됨,", posX, posY);
+      return;
+    }
+
+    if(mainarr[posY*10+posX].clicked){
+      console.log('이미 클릭됨',posX,posY);
+      return;
+    }
+    // 닫힌 칸이 아니면 리턴
+
+    console.log(checked.find((index) => index === posY * 10 + posX));
+    if (checked.find((index) => index === posY * 10 + posX) === undefined)
+      checked.push(posY * 10 + posX); //중복방지
+    console.log(checked);
+
+    let count = 0;
+    for (let i = -1; i < 2; i++) {
+      for (let j = -1; j < 2; j++) {
+        if (posX + j >= 0 && posX + j <= 9 && posY + i >= 0 && posY + i <= 9) {
+          if (mainarr[(posY + i) * 10 + posX + j].number === -1) {
+            console.log(posX+j,posY+i,'폭탄 감지됨.')
+            count++;
+          }
+        }
+      }
+    }
+    if (!count) {
+      for (let i = -1; i < 2; i++) {
+        for (let j = -1; j < 2; j++) {
+          if (posX + j >= 0 && posX + j <= 9 && posY + i >= 0 && posY + i <= 9)
+          if(j===0 && i === 0){
+
+          }
+          else{
+            findEmpty(posX + j, posY + i);
+
+          }
+        }
+      }
+    }
+
+    return;
+  }
+
+  const onClick = (posX, posY) => {
+    if (mainarr[posY * 10 + posX].number === -1) {
+      alert("폭탄 받아라!");
+    }
+    findEmpty(posX, posY);
+    /* setArr(
+      mainarr.map((index) =>
+        index.pos === posX + "," + posY
+          ? { ...index, clicked: true }
+          : { ...index }
+      )
+    ); */
+    setArr(mainarr.map(
+      index=> index.index === checked.find(value=> value === index.index) ? {...index,clicked:true} : {...index}
+    ))
+    console.log(mainarr[posY * 10 + posX].index);
+    console.log(posX, posY);
+    console.log(mainarr[posY * 10 + posX].pos);
+    console.log(mainarr[posY * 10 + posX].clicked);
+    console.log(mainarr[posY * 10 + posX].number);
+    return;
+  };
+
+  return (
+    <>
+      <Board arr={mainarr} onClick={onClick} />
+    </>
+  );
 }
 
 export default App;
--- React/mine-find/src/Board.js
+++ React/mine-find/src/Board.js
@@ -1,68 +1,46 @@
 import React, { useState } from "react";
-import { render } from "react-dom";
 import { S } from "./styles";
 
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min; //최댓값은 제외, 최솟값은 포함
-}
+function Board(props) {
+  function rendering() {
+    const result = [];
+    const count = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-function addBomb(posX,posY,arr){
-    
-        
-       
-        arr[posY*10+posX].number = -1;
-        console.log(`X : ${posX} Y : ${posY}`);
-        
-    
-}
-function findBomb(X,Y,arr){
-    for(let i=0;i<3;i++){
-        for (let j=0;j<3;j++){
-            if (Y+(i-1)<0 || Y+(i-1)>9 || X+(j-1)<0 || X+(j-1)>9){
-
-            }
-            else{
-                if (arr[Y+(i-1)][X+(j-1)].number!== -1){
-                    arr[Y+(i-1)][X+(j-1)].number += 1;
-                }
-            }
-        }
+    const style_bomb = {
+        backgroundColor:'red'
     }
-}
-
-function Board(props){
-   
+    const style_default = {
+        backgroundColor:'lightgray'
+    }
     
-    const rendering= () => {
-        const result= [];
-        for (let i=0;i<10;i++){
-            const temp = [];
-            for (let j=0;j<10;j++){
-                temp.push(<S.Block>{props.array[i][j]}</S.Block>);
-            }
-            result.push(<S.Row>{temp}</S.Row>)
-        }
-        return result;
-
+    const style_clicked = {
+        backgroundColor:'gray'
     }
 
-    const result= [];
-    const temp = [];
-        for (let i=0;i<10;i++){
-            for (let j=0;j<10;j++){
-                temp.push(<S.Block key={j+','+i}>{props.array[i][j]}</S.Block>);
-            }
-            result.push(<S.Row>{temp}</S.Row>)
-        }
-    return(    
-        <div>
-        {temp}    
-        </div>
-    )
-   
-}
+    for (let i = 0; i < 10; i++) {
+      const temp = (
+        <S.Row>
+          {count.map((index) => (
+            <S.Block style={props.arr[10*i+index].clicked ? props.arr[10*i+index].number === -1 ? style_bomb : style_clicked : style_default}
+              index={props.arr[10 * i + index].number}
+              onClick={() => {
+                props.onClick(index, i);
+              }}
+            >
+              {props.arr[10 * i + index].number > 0 &&
+              props.arr[10 * i + index].clicked
+                ? props.arr[10 * i + index].number
+                : ""}
+            </S.Block>
+          ))}
+        </S.Row>
+      );
 
+      result.push(temp);
+    }
+    return result;
+  }
+  return rendering();
+}
 
 export default Board;
