81e5d64b932463b804a1a1c17fd3c06b76921a18
justinlee05 <justinlee05@naver.com>
2021-10-20 23:45:32 +0900

    +

--- React/begin-react/src/components/InputSample.js
+++ React/begin-react/src/components/InputSample.js
@@ -1,30 +1,28 @@
 import React, { useRef, useState } from 'react';
 
 function InputSample() {
-  const [name, setName] = useState('');
-  const [nickname, setNickname] = useState('');
+  const [inputs, setInputs] = useState({
+      name:"",
+      nickname:""
+  });
 
+  const {name , nickname} = inputs;
   const onChange = (e) => {
+    console.log(e.target.name);
     console.log(e.target.value);
-    setName(e.target.value);
-  };
-  const onChange1 = (e) => {
-    console.log(e.target.value);
-    setNickname(e.target.value);
   };
 
   const onReset = () => {
-    setName('');
-    setNickname('');
+
   };
 
   return (
     <div>
-      <input onChange={onChange} value={name} placeholder="이름" />
-      <input onChange={onChange1} value={nickname} placeholder="닉네임" />
+      <input onChange={onChange} name="name" value={name} placeholder="이름" />
+      <input onChange={onChange} name="nickname" value={nickname} placeholder="닉네임" />
       <button onClick={onReset}>초기화</button>
       <div>
-        <b>값:{name + '(' + nickname + ')'}</b>
+        <b>값:{name}({nickname})</b>
       </div>
     </div>
   );
