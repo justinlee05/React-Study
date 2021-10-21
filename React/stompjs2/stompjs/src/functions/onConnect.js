88eab29f825021c20bade20ca9e3928a14abe750
justinlee05 <justinlee05@naver.com>
2021-10-21 11:38:10 +0900

    stomp 포기
    socket.io로 갈아탑니다

--- React/stompjs2/stompjs/src/functions/onConnect.js
+++
@@ -1,9 +0,0 @@
-import React, { useEffect, useState, useRef } from "react";
-import { Stomp } from "@stomp/stompjs";
-import { wsurl } from "../App";
-import SockJsClient from "react-stomp";
-export const SocketTest = () => {
-  const wsSourceUrl = "http://13.125.0.94:9094/websocket/";
-  return(<> </>)
-};
-
