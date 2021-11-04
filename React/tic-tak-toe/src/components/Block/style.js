7e05f21b4b8464fed807576556be4dc9bc98d083
justinlee05 <justinlee05@naver.com>
2021-11-04 17:22:45 +0900

    스타일

--- React/tic-tak-toe/src/components/Block/style.js
+++ React/tic-tak-toe/src/components/Block/style.js
@@ -4,6 +4,8 @@ export const S ={
     BlockWrapper:styled.div`
     width:50px;
     height:50px;
-
+    &:hover{
+        background-color:rgb(128,128,128,0.5)
+    }
     `
 }
--- React/tic-tak-toe/src/components/Board/style.js
+++ React/tic-tak-toe/src/components/Board/style.js
@@ -25,4 +25,29 @@ export const S = {
     position:absolute;
     background-color:rgb(0,0,0,0);
     `,
+    Turn:styled.div`
+    border:5px solid gray;
+    font-size:30px;
+    position:absolute;
+    border-radius:10px;
+    height:100px;
+    width:900px;
+    display:flex;
+    align-items:center;
+    justify-content:space-around;
+    background-color:#DCB36A;
+    font-weight:bold;
+
+    `,
+    TurnText:styled.span`
+        padding:10px;
+        border-radius:20px;
+    `,
+    PassButton:styled.button`
+    width:200px;
+    height:60px;
+    border:none;
+    font-size:40px;
+    border-radius:20px;
+    `,
 }
