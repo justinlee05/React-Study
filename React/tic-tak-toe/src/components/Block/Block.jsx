edb6c295c6ab8aeeaf8df9cf1f53055d3e122863
justinlee05 <justinlee05@naver.com>
2021-11-04 17:22:35 +0900

    바둑판 렌더링에 쓰일 블록 컴포넌트

--- React/tic-tak-toe/src/components/Block/Block.jsx
+++ React/tic-tak-toe/src/components/Block/Block.jsx
@@ -1,35 +1,41 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { S } from "./style";
 
+function Block({ setBlock, text, border, pos, num, checkWinner }) {
+  text = text === undefined ? 0 : text;
+  const bstyle = {
+    border: "1px solid black",
+  };
+  const nstyle = {
+    margin: "1.2px",
+    borderRadius: "100%",
+  };
+  const black = {
+    backgroundColor: "black",
+  };
+  const white = {
+    backgroundColor: "white",
+  };
 
-function Block({setBlock,text,border,pos,num}){
+  const onClick = () => {
+    setBlock(pos.x, pos.y);
+    checkWinner(pos.x, pos.y);
+  };
 
-    text = text===undefined?0:text;
-
-    useEffect(()=>{
-        if(pos){
-        
-        }
-    })
-
-    const bstyle = {
-        border:'1px solid black'
-    }
-    const nstyle = {
-        margin:'1.2px',
-        borderRadius:"100%",
-    }
-    const black = {
-        backgroundColor:'black'
-    }
-    const white = {
-        backgroundColor:'white'
-    }
-    return(
-        <S.BlockWrapper style={border ? bstyle : num===1 ? {...nstyle,...black} : num===2 ? {...nstyle,...white} : nstyle} onClick={()=>setBlock(pos.x,pos.y)}>
-            
-        </S.BlockWrapper>
-    )
+  return (
+    <S.BlockWrapper
+      style={
+        border
+          ? bstyle
+          : num === 1
+          ? { ...nstyle, ...black }
+          : num === 2
+          ? { ...nstyle, ...white }
+          : nstyle
+      }
+      onClick={() => {onClick()}}
+    ></S.BlockWrapper>
+  );
 }
 
 export default Block;
